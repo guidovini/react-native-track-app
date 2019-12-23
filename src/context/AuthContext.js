d5bcd0f2a43107b84a1343c26e330a71c0d57173
Guido Santillan <guidosantillan01@gmail.com>
2019-12-23 13:06:44 +0100

    Define AuthProvider actions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,33 @@ const authReducer = (state, action) => {
   }
 };
 
-const actions = {};
+const signup = dispatch => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    // if signing up fails, we probably need to reflect and error message somewhere
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // somehow sign out
+  };
+};
+
+const actions = {
+  signup,
+  signin,
+  signout
+};
 
 const initialValue = {
   isSignedIn: false
