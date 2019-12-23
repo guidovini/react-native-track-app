26d75a37de2d64a475b456b0fde86247596cae6b
Guido Santillan <guidosantillan01@gmail.com>
2019-12-23 12:39:38 +0100

    Add some styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
@@ -16,10 +16,24 @@ const SignupScreen = ({ navigation }) => {
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'red',
+    borderWidth: 10,
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200
+  }
+});
 
 export default SignupScreen;
