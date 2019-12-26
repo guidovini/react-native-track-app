abc92b6032f6e8c579e7dc8df714da3b2b63d0a8
Guido Santillan <guidosantillan01@gmail.com>
2019-12-26 23:09:11 +0100

    Empty Screens while resolving Auth

--- App.js
+++ App.js
@@ -9,24 +9,31 @@ import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { setNavigator } from './src/navigationRef';
 
-const switchNavigator = createSwitchNavigator({
-  loginFlow: createStackNavigator({
-    Signup: SignupScreen,
-    Signin: SigninScreen
-  }),
-  mainFlow: createBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen
+const switchNavigator = createSwitchNavigator(
+  {
+    ResolveAuth: ResolveAuthScreen,
+    loginFlow: createStackNavigator({
+      Signup: SignupScreen,
+      Signin: SigninScreen
     }),
-    TrackCreate: TrackCreateScreen,
-    Account: AccountScreen
-  })
-});
+    mainFlow: createBottomTabNavigator({
+      trackListFlow: createStackNavigator({
+        TrackList: TrackListScreen,
+        TrackDetail: TrackDetailScreen
+      }),
+      TrackCreate: TrackCreateScreen,
+      Account: AccountScreen
+    })
+  },
+  {
+    initialRouteName: 'ResolveAuth'
+  }
+);
 
 const App = createAppContainer(switchNavigator);
 
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,20 +1,14 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 
-import { Context } from '../context/AuthContext';
+import { Context as AuthContext } from '../context/AuthContext';
 
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SigninScreen = ({ navigation }) => {
-  const { state, signin, clearErrorMessage, tryLocalSignin } = useContext(
-    Context
-  );
-
-  useEffect(() => {
-    tryLocalSignin();
-  }, []);
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
