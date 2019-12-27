097412c47c94d2eed09d1d330cc071a589f76059
Guido Santillan <guidosantillan01@gmail.com>
2019-12-27 15:02:45 +0100

    Building a Track Form

---
+++ src/components/TrackForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Input, Button } from 'react-native-elements';
+
+import Spacer from './Spacer';
+
+const TrackForm = () => {
+  return (
+    <>
+      <Spacer>
+        <Input placeholder="Enter name" />
+      </Spacer>
+      <Button title="Start recording" />
+    </>
+  );
+};
+
+export default TrackForm;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,6 +9,7 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 import Map from '../components/Map';
 import useLocation from '../hooks/useLocation';
+import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
@@ -19,6 +20,7 @@ const TrackCreateScreen = ({ isFocused }) => {
       <Text h2>Create a Track</Text>
       <Map />
       {err && <Text>Please enable location services</Text>}
+      <TrackForm />
     </SafeAreaView>
   );
 };
