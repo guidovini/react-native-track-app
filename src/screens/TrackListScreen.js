94801d9f03bf7f3fa9d62cc0480c2c1c145a3c2b
Guido Santillan <guidosantillan01@gmail.com>
2019-12-27 18:20:59 +0100

    Listing saved tracks

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { NavigationEvents } from 'react-navigation';
+
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
   return (
     <>
-      <Text>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate('TrackDetail')}
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem chevron title={item.name} />
+            </TouchableOpacity>
+          );
+        }}
       />
     </>
   );
