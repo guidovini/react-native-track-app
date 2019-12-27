24c5ede0a6bac5a656f40c5782c9a394a05abc00
Guido Santillan <guidosantillan01@gmail.com>
2019-12-27 16:04:10 +0100

    Cleaning Up After Ourselves

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -34,7 +34,13 @@ export default (shouldTrack, callback) => {
       subscriber.remove();
       setSubscriber(null);
     }
-  }, [shouldTrack]);
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback]);
 
   return [err];
 };
