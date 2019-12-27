42be066dd20c751309feb08170e2306fdae0baa2
Guido Santillan <guidosantillan01@gmail.com>
2019-12-27 16:15:31 +0100

    Avoiding Stale References

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,32 +7,34 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (error) {
-      setErr(error);
-    }
-  };
 
   useEffect(() => {
+    let subscriber;
+
+    const startWatching = async () => {
+      try {
+        await requestPermissionsAsync();
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          callback
+        );
+      } catch (error) {
+        setErr(error);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
       // stop watching
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
     return () => {
